d0053d4f04becc67188b7759c9db87aaf17c8199
gokhan <gkhnznkldk@gmail.com>
2022-09-09 17:24:18 +0300

    09,09,2022

--- src/test/java/driver/Driver.java
+++ src/test/java/driver/Driver.java
@@ -2,17 +2,31 @@ package driver;
 
 import com.thoughtworks.gauge.AfterScenario;
 import com.thoughtworks.gauge.BeforeScenario;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
     // Holds the WebDriver instance
     public static WebDriver webDriver;
-
-    // Gerekli tarayıcının bir webDriver örneğini başlat
-    // Bunun uygulamanın iş etki alanında bir önemi olmadığından, webDriver'ı başlatmak için BeforeSuite kancası kullanılır
+    static final String HOST_URL = "http://localhost:4444/wd/hup";
+    public static String platformdocker = "docker";
     @BeforeScenario
-    public void initializeDriver(){webDriver = DriverFactory.getDriver();}
+    public void initializeDriver() throws MalformedURLException {
+
+
+               webDriver = DriverFactory.getDriver();
+
+
+
+
+    }
 
     // Close the webDriver instance
     @AfterScenario
